0b26fd96495f792380a50c8d3571c343ee2f0dc5
Damian Soto <soto.platero@gmail.com>
2025-02-20 23:36:32 -0500

    update

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -1,11 +1,13 @@
 // /src/routes/sitemap.xml/+server.js
 import * as sitemap from 'super-sitemap';
+import { commonSpecies, iconicTaxa } from '$lib/iconicTaxa';
 
 export const GET = async () => {
   return await sitemap.response({
     origin: 'https://randomanimal.site',
     paramValues: {
-      "/taxonomy/[taxo]": ['mammalia', 'aves', 'reptilia', 'amphibia', 'actinopterygii', 'mollusca', 'arachnida', 'insecta']
+      "/taxonomy/[taxo]": iconicTaxa.map(t => t.name.toLowerCase()),
+      "/species/[specie]": commonSpecies.map(s => s.title.toLowerCase()),
     }
   });
 };
